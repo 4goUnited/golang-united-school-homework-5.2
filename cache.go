d428a07b079e9099be6df848420274a467cf3e27
4goUnited <4goUnited@gmail.com>
2022-06-03 21:39:13 +0500

    Testing first solution

--- cache.go
+++ cache.go
@@ -1,23 +1,53 @@
 package cache
 
-import "time"
+import (
+//	"fmt"
+	"time"
+)
+
+type vt struct {
+	v string
+	t time.Time
+}
 
 type Cache struct {
+	m map[string]vt
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{map[string]vt{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	if vt, ok := c.m[key]; !ok {
+		return "", false 
+	} else if time.Now().Before(vt.t) {
+		return "", false
+	}
+	return c.m[key].v, true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.m[key] = vt{v: value, t: time.Unix(1<<63-1,0)}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	sl := []string{}
+	for key, _ := range c.m {
+		sl = append(sl, key)
+	}
+	return sl
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.Update()
+	c.m[key] = vt{v: value, t: deadline}
+}
+
+func (c *Cache) Update() {
+	for key, vt := range c.m {
+		if time.Now().Before(vt.t) {
+			delete(c.m, key)
+		}
+	}
 }
